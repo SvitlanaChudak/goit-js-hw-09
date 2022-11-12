0ee176e3589f8819532e4da4e2fd691fd8613ddf
SvitlanaChudak <svit.lemon@gmail.com>
2022-11-12 20:56:40 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+function getRandomHexColor() {
+body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(getRandomHexColor, 1000);
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+});
+
+
+
+
+
 
