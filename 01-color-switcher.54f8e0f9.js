00a3f7c1a33a94220250610d9c3dd908ea13b9c0
SvitlanaChudak <106378776+SvitlanaChudak@users.noreply.github.com>
2022-11-12 18:57:05 +0000

    Deploying to gh-pages from @ SvitlanaChudak/goit-js-hw-09@0ee176e3589f8819532e4da4e2fd691fd8613ddf 🚀

---
+++ 01-color-switcher.54f8e0f9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;function o(){r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{d=setInterval(o,1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.54f8e0f9.js.map
